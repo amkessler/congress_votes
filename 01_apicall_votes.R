24623aa16b86a3a9efa8eb77d57daf58f8eed8d3
amkessler <kessler.aaron@gmail.com>
2019-09-16 14:41:30 -0400

    remove extraneous code now that function written

--- 01_apicall_votes.R
+++ 01_apicall_votes.R
@@ -6,46 +6,6 @@ library(curl)
 options(scipen = 999)
 options(stringsAsFactors = FALSE)
 
-####get member information ####
-
-get_memberinfo <- GET("https://api.propublica.org/congress/v1/116/house/members.json", 
-                    add_headers(`X-API-Key` = Sys.getenv("PROPUBLICA_API_KEY"))) #key stored as environ variable
-
-get_memberinfo$status_code
-
-get_memberinfo$content
-#convert from raw to characters
-this.raw.content <- rawToChar(get_memberinfo$content)
-#count how many characters
-nchar(this.raw.content)
-#look at first 100
-substr(this.raw.content, 1, 100)
-#parse this json
-this.content <- fromJSON(this.raw.content)
-#returns a list?
-class(this.content)
-#how long is the list
-length(this.content)
-this.content[[1]] #the first element - should be states if working
-this.content[[3]] #the data itself, or so it appears to be
-#dataframe from JUST the 3 content 
-content3_df <- as.data.frame(this.content[[3]])
-#inspect the list column named "members" for one record
-str(content3_df$members[[1]], max.level = 1)
-#unnest
-z <- content3_df %>%
-  unnest()
-result_memberlist <- z
-
-
-#see if any ids repeated
-result_memberlist %>% 
-  count(id) %>% 
-  filter(n > 1)
-
-#save result
-saveRDS(result_memberlist, "processed_data/result_memberlist.rds")
-
 
 
 #### specific vote call ####
