e2101aa95a0eea8524cd721350d357a3813f4ab6
amkessler <kessler.aaron@gmail.com>
2019-09-16 14:13:55 -0400

    add function for bill cosponsors

--- 00_functions.R
+++ 00_functions.R
@@ -8,13 +8,13 @@ options(stringsAsFactors = FALSE)
 
 
 
-#### get member information ####
+#### GET MEMBER INFORMATION ####
 
 ppapi_download_memberinfo <- function(congress_session, chamber) {
   
-  myurl <- paste0("https://api.propublica.org/congress/v1/", congress_session, "/", chamber, "/members.json")
+  memberlist_url <- paste0("https://api.propublica.org/congress/v1/", congress_session, "/", chamber, "/members.json")
   
-  get_memberinfo <- GET(myurl, 
+  get_memberinfo <- GET(memberlist_url, 
                         add_headers(`X-API-Key` = Sys.getenv("PROPUBLICA_API_KEY"))) #key stored as environ variable
   this.raw.content <- rawToChar(get_memberinfo$content)
   this.content <- fromJSON(this.raw.content)
@@ -26,4 +26,27 @@ ppapi_download_memberinfo <- function(congress_session, chamber) {
 
   return(result_memberlist)
   
+}
+
+
+
+#### SPECIFIC BILL - LIST OF COSPONSORS #### 
+
+ppapi_download_cosponsors <- function(congress_session, billnum) {
+  
+  cosponsor_url <- paste0("https://api.propublica.org/congress/v1/", congress_session, "/bills/", billnum, "/cosponsors.json")
+
+  get_cosponsors <- GET(cosponsor_url, 
+                        add_headers(`X-API-Key` = Sys.getenv("PROPUBLICA_API_KEY")))
+  
+  this.raw.content <- rawToChar(get_cosponsors$content)
+  #parse this json
+  this.content <- fromJSON(this.raw.content)
+  content3_df <- as_tibble(this.content[[3]])
+  #unnest
+  result_cosponsors <- content3_df %>%
+    unnest(cosponsors)
+  
+  return(result_cosponsors)
+
 }
