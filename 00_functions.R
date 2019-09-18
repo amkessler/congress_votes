61632667a377fefbd796c040520274a6706df09d
amkessler <kessler.aaron@gmail.com>
2019-09-18 15:14:58 -0400

    add function for roll call votes

--- 00_functions.R
+++ 00_functions.R
@@ -50,3 +50,40 @@ ppapi_download_cosponsors <- function(congress_session, billnum) {
   return(result_cosponsors)
 
 }
+
+
+
+
+#### VOTEs ON A BILL #### 
+
+ppapi_download_rollcallvote <- function(congress_session, chamber, session, voteid) {
+  
+  votes_url <- paste0("https://api.propublica.org/congress/v1/", congress_session, "/", chamber, "/sessions/", session, "/votes/", voteid, ".json")
+
+  get_rollcall <- GET(votes_url, 
+                      add_headers(`X-API-Key` = Sys.getenv("PROPUBLICA_API_KEY")))
+  
+  this.raw.content <- rawToChar(get_rollcall$content)
+  this.content <- fromJSON(this.raw.content)
+  this.content[[3]] #the data itself, or so it appears to be
+  content3_df <- as_tibble(this.content[[3]])
+  result_rollcall <- content3_df$votes$vote$positions %>%
+    unnest()
+  
+  #create a new column for house_dist matching with census/elex data
+  distcorrect <- if_else(str_length(result_rollcall$district)==1,
+                         paste0("0",result_rollcall$district),
+                         result_rollcall$district)
+  
+  result_rollcall$distcorrect <- distcorrect
+  result_rollcall$house_dist <- paste0(result_rollcall$state, "-", result_rollcall$distcorrect)
+  
+  result_rollcall <- result_rollcall %>% 
+    select(-distcorrect, -dw_nominate)
+  
+  return(result_rollcall)
+  
+}
+
+  
+  
